2e59da4dc792a90bde5f373b0c1a06523733c4c7
Joe Ridenour <jridenour@mercury.io>
2015-03-19 20:46:54 -0500

    stuff

--- Examples/BasicExample/Classes/CustomShizzyVC.m
+++ Examples/BasicExample/Classes/CustomShizzyVC.m
@@ -22,6 +22,7 @@
 @implementation ForgotPass @end
 
 @interface Reg1 : NSObject <FXForm>
+@property (nonatomic, assign) BOOL isSectioned; // Determines if there's more than 1 section
 @property (nonatomic, copy) NSString *firstName;
 @property (nonatomic, copy) NSString *lastName;
 @property (nonatomic, copy) NSString *username;
@@ -29,7 +30,23 @@
 @property (nonatomic, copy) NSString *password;
 @property (nonatomic, copy) NSString *passwordConfirm;
 @end
-@implementation Reg1 @end
+@implementation Reg1
+
+- (id)birthDayField {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    dict[FXFormFieldKey] = @"birthDay";
+    dict[FXFormFieldType] = FXFormFieldTypeDate;
+    dict[FXFormFieldPlaceholder] = @"Enter birthday...";
+    if (self.isSectioned) {
+        dict[FXFormFieldHeader] = @"Section header here";
+    }
+    return [dict copy];
+}
+
+- (NSArray *)excludedFields {
+    return @[@"isSectioned"];
+}
+@end
 
 
 
@@ -54,35 +71,19 @@
     NSMutableArray *updates = [NSMutableArray array];
     NSMutableArray *deletes = [NSMutableArray array];
     
+    NSMutableIndexSet *addedIndexSet = [NSMutableIndexSet indexSet];
+    NSMutableIndexSet *removedIndexSet = [NSMutableIndexSet indexSet];
     
     // First we want to figure out what the deletes and updates are
     NSInteger section = 0, item = 0;
-    for (FXFormSection *sec in fc1.sections) {
-        
-        for (FXFormField *f in sec.fields) {
-            NSIndexPath *curIp = [NSIndexPath indexPathForItem:item inSection:section];
-            
-            // Check fc2 for this field
-            if ([fc2 fieldForKey:f.key]) {
-                NSIndexPath *otherIP = [fc2 indexPathForField:[fc2 fieldForKey:f.key]];
-                if ([curIp isEqual:otherIP]) {
-                    // No update
-                }
-                else {
-                    [updates addObject:@{@"ip": otherIP, @"field": f, @"oldPath": curIp}];
-                }
-            }
-            else {
-                // This field is deleted.   Peace
-                [deletes addObject:@{@"ip": curIp, @"field": f}];
-            }
-            item++;
-        }
-        section++;
-    }
-    
-    section = 0, item = 0;
     for (FXFormSection *sec in fc2.sections) {
+        
+        if (section >= fc1.sections.count) {
+            // We're inserting a new section.
+            [addedIndexSet addIndex:section];
+            [inserts addObject:@{@"sec": [NSIndexPath indexPathForItem:NSNotFound inSection:section]}];
+            continue;
+        }
         
         for (FXFormField *f in sec.fields) {
             NSIndexPath *curIp = [NSIndexPath indexPathForItem:item inSection:section];
@@ -94,6 +95,54 @@
             
             item++;
         }
+        section++;
+    }
+    
+    section = 0, item = 0;
+    for (FXFormSection *sec in fc1.sections) {
+        BOOL removedSection = NO;
+        if (section >= fc2.sections.count) {
+            // We're deleting a new section.
+            [removedIndexSet addIndex:section];
+            [deletes addObject:@{@"sec": [NSIndexPath indexPathForItem:NSNotFound inSection:section]}];
+            removedSection = YES;
+        }
+        
+        item = 0;
+        for (FXFormField *f in sec.fields) {
+            NSIndexPath *curIp = [NSIndexPath indexPathForItem:item inSection:section];
+            
+            // Check fc2 for this field
+            if ([fc2 fieldForKey:f.key]) {
+                NSIndexPath *otherIP = [fc2 indexPathForField:[fc2 fieldForKey:f.key]];
+                
+                if (removedSection) {
+                    [inserts addObject:@{@"ip": otherIP}];
+                    NSLog(@"Insert: %@", f.key);
+                    continue;
+                }
+                
+                FXFormSection *newSec = fc2.sections[section];
+                if ([curIp isEqual:otherIP]) {
+                    // No update
+                }
+                else if (curIp.section == otherIP.section) {
+                    [updates addObject:@{@"ip": otherIP, @"field": f, @"oldPath": curIp}];
+                }
+                else if (item >= newSec.fields.count) {
+                    [deletes addObject:@{@"ip": curIp}];
+                }
+                else if (!removedSection) {
+                    [deletes addObject:@{@"ip": curIp}];
+                }
+            }
+            else {
+                // This field is deleted.   Peace
+                [deletes addObject:@{@"ip": curIp, @"field": f}];
+            }
+            item++;
+        }
+        
         section++;
     }
     
@@ -126,17 +175,46 @@
     NSArray *deletes = diffs[@"deletes"];
     NSArray *inserts = diffs[@"inserts"];
     
+    NSMutableArray *insertedIndexPaths = [NSMutableArray array];
+    NSMutableIndexSet *insertedSections = [NSMutableIndexSet indexSet];
+    for (NSDictionary *i in inserts) {
+        if (i[@"sec"]) {
+            [insertedSections addIndex:[i[@"sec"] section]];
+        }
+        else {
+            [insertedIndexPaths addObject:i[@"ip"]];
+        }
+    }
+    NSMutableArray *deletedIndexPaths = [NSMutableArray array];
+    NSMutableIndexSet *deletedSections = [NSMutableIndexSet indexSet];
+    for (NSDictionary *i in deletes) {
+        if (i[@"sec"]) {
+            [deletedSections addIndex:[i[@"sec"] section]];
+        }
+        else {
+            [deletedIndexPaths addObject:i[@"ip"]];
+        }
+    }
+    
     [self.collectionView performBatchUpdates:^{
         self.formController.form = form;
         
-        if (deletes.count > 0)
-            [self.collectionView deleteItemsAtIndexPaths:[deletes valueForKeyPath:@"@unionOfObjects.ip"]];
-        if (inserts.count > 0) {
-            [self.collectionView insertItemsAtIndexPaths:[inserts valueForKeyPath:@"@unionOfObjects.ip"]];
+        if (deletedIndexPaths.count > 0) {
+            [self.collectionView deleteItemsAtIndexPaths:deletedIndexPaths];
+        }
+        if ([deletedSections count] > 0) {
+            [self.collectionView deleteSections:deletedSections];
+        }
+        if (insertedIndexPaths.count > 0) {
+            [self.collectionView insertItemsAtIndexPaths:insertedIndexPaths];
+        }
+        if ([insertedSections count] > 0) {
+            [self.collectionView insertSections:insertedSections];
         }
         
-    } completion:^(BOOL finished) {
         
+    } completion:^(BOOL finished) {
+        [self.collectionView reloadData];
     }];
 }
 
@@ -152,6 +230,11 @@
     [self performCollectionUpdatesWithForm:[Reg1 new]];
 }
 
+- (IBAction)reg2:(id)sender {
+    Reg1 *f = [Reg1 new];
+    f.isSectioned = YES;
+    [self performCollectionUpdatesWithForm:f];
+}
 @end
 
 
